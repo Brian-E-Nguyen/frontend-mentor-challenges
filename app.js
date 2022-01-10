47f1172b1bf6740a09b71ddb62138ef44eea2dc4
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-09 17:55:53 -0800

    app.js: card and image

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log('test');
+const cardData = [
+  {
+    title: 'Profile Card',
+    image: './Profile-Card/design/desktop-design.jpg',
+    siteSrc: './Profile-Card/index.html',
+    githubSrc:
+      'https://github.com/Brian-E-Nguyen/frontend-mentor-challenges/tree/master/Profile-Card',
+  },
+];
+
+// cardData.map((data, i) => {
+//   var card = document.createElement('div');
+//   card.innerHTML = (
+//     <div class='challenge-card'>
+//       <img
+//         src='./Profile-Card/design/desktop-design.jpg'
+//         class='challenge-card__banner'
+//       />
+//     </div>
+//   );
+//   document.getElementsByClassName('container')[0].appendChild(card);
+//   console.log(i);
+// });
+
+cardData.forEach((data) => {
+  var card = document.createElement('div');
+  card.innerHTML =
+    `<div class='challenge-card'>` +
+    `<img  src='${data.image}' class='challenge-card__banner'/>` +
+    `</div>`;
+  document.getElementsByClassName('container')[0].appendChild(card);
+});
