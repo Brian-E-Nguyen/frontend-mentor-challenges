01f4569e32cd8279f9fb684ac74586c0b9c7f48d
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-11 20:44:24 -0800

    error message active state

--- Sign-Up-Form/script.js
+++ Sign-Up-Form/script.js
@@ -1,11 +1,7 @@
-// document
-//   .getElementsByClassName('signup-form__button')[0]
-//   .addEventListener('click', (event) => {
-//      event.preventDefault();
-//   });
-
+const form = document.getElementsByClassName('signup-form__container')[0];
+console.log(form);
 const validateForm = (event) => {
-  console.log(event);
+  event.preventDefault();
   const firstName = document.getElementById('firstName').value;
   const lastName = document.getElementById('lastName').value;
   const email = document.getElementById('email').value;
@@ -20,9 +16,15 @@ const validateForm = (event) => {
 
   checkInputLength(formData);
 };
+form.addEventListener('submit', validateForm);
 
 const checkInputLength = (data) => {
-  if (!data.firstName.length) console.log(data.firstName);
+  if (!data.firstName.length) {
+    var errorFirstNameElement = document.getElementsByClassName(
+      'signup-form__error first-name'
+    )[0];
+    errorFirstNameElement.classList.add('active');
+  }
   if (!data.lastName.length) console.log(data.lastName);
   if (!data.email.length) console.log(data.email);
   if (!data.password.length) console.log(data.password);
