5cdba45ad4cbef78c0f2ee98c649fd4463e909e7
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-11 20:26:01 -0800

    starting form validation

--- Sign-Up-Form/script.js
+++ Sign-Up-Form/script.js
@@ -1,10 +1,29 @@
-const validateForm = () => {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-}
+// document
+//   .getElementsByClassName('signup-form__button')[0]
+//   .addEventListener('click', (event) => {
+//      event.preventDefault();
+//   });
 
-const checkInputLength = (field) => {
-    return field.length ? true : false;
-}
+const validateForm = (event) => {
+  console.log(event);
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  const formData = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password,
+  };
+
+  checkInputLength(formData);
+};
+
+const checkInputLength = (data) => {
+  if (!data.firstName.length) console.log(data.firstName);
+  if (!data.lastName.length) console.log(data.lastName);
+  if (!data.email.length) console.log(data.email);
+  if (!data.password.length) console.log(data.password);
+};
