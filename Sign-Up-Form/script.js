e5fbc28b65b1a14269cadb4c4b94db0a650fa18a
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-12 13:39:33 -0800

    validation functions now return boolean

--- Sign-Up-Form/script.js
+++ Sign-Up-Form/script.js
@@ -17,43 +17,53 @@ form.addEventListener('submit', validateForm);
 const validateFirstName = (name) => {
   let errorFirstNameElement = document.getElementById('first-name-error');
   let firstNameInput = document.getElementById('firstName');
+  let isValidName = false;
 
   if (name.length) {
     errorFirstNameElement.classList.remove('active');
     errorFirstNameElement.innerText = '';
     firstNameInput.classList.remove('input-error');
+    isValidName = true;
   } else {
     errorFirstNameElement.classList.add('active');
     errorFirstNameElement.innerText = 'First Name cannot be empty';
     firstNameInput.classList.add('input-error');
   }
+
+  return isValidName;
 };
 
 const validateLastName = (name) => {
   let errorLastNameElement = document.getElementById('last-name-error');
   let lastNameInput = document.getElementById('lastName');
+  let isValidName = false;
 
   if (name.length) {
     errorLastNameElement.classList.remove('active');
     errorLastNameElement.innerText = '';
     lastNameInput.classList.remove('input-error');
+    isValidName = true;
   } else {
     errorLastNameElement.classList.add('active');
     errorLastNameElement.innerText = 'First Name cannot be empty';
     lastNameInput.classList.add('input-error');
   }
+
+  return isValidName;
 };
 
 const validateEmail = (email) => {
   const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   let emailInput = document.getElementById('email');
   let errorEmailElement = document.getElementById('email-error');
+  let isValidEmail = false;
 
   if (email.length) {
     if (email.match(mailFormat)) {
       errorEmailElement.classList.remove('active');
       emailInput.classList.remove('input-error');
       errorEmailElement.innerText = '';
+      isValidEmail = true;
     } else {
       errorEmailElement.classList.add('active');
       emailInput.classList.add('input-error');
@@ -64,19 +74,25 @@ const validateEmail = (email) => {
     emailInput.classList.add('input-error');
     errorEmailElement.innerText = 'Email cannot be empty';
   }
+
+  return isValidEmail;
 };
 
 const validatePassword = (password) => {
   let errorPasswordElement = document.getElementById('password-error');
   let passwordInput = document.getElementById('password');
+  let isValidPassword = false;
 
   if (password.length) {
     errorPasswordElement.classList.remove('active');
     errorPasswordElement.innerText = '';
     passwordInput.classList.remove('input-error');
+    isValidPassword = true;
   } else {
     errorPasswordElement.classList.add('active');
     errorPasswordElement.innerText = 'Password cannot be empty';
     passwordInput.classList.add('input-error');
   }
+
+  return isValidPassword;
 };
