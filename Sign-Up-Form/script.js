03241c174268a0c1b35add4cf44c4311a1268102
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-11 16:58:38 -0800

    retrieve inputs

---
+++ Sign-Up-Form/script.js
@@ -0,0 +1,8 @@
+const validateForm = () => {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    
+    alert(firstName + lastName + email + password)
+}
