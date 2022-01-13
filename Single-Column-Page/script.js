82a8642cb19dfb2e2dcf499597a8b03d1c4ce2a2
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-13 11:32:28 -0800

    styles for errors

--- Single-Column-Page/script.js
+++ Single-Column-Page/script.js
@@ -2,18 +2,21 @@ const form = document.getElementsByTagName('form')[0];
 
 const validateEmail = (event) => {
   event.preventDefault();
-  let email = document.getElementById('email').value;
+  let email = document.getElementById('input-box__email');
   let isValidEmail = false;
   const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (email.length) {
+  if (email.value.length) {
     if (email.match(emailFormat)) {
       isValidEmail = true;
     } else {
     }
   } else {
+    let inputBox = document.getElementById('input-box__email');
     let errorMessage = document.getElementById('error-message');
     errorMessage.classList.add('active');
     errorMessage.innerText = 'Field cannot be blank';
+
+    inputBox.classList.add('active-error');
   }
   return isValidEmail;
 };
