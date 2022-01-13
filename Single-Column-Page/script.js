45ba5ff92575eb413205b3f82bcd5487030baaed
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-13 11:02:52 -0800

    validateEmail basic structure

--- Single-Column-Page/script.js
+++ Single-Column-Page/script.js
@@ -1,7 +1,20 @@
 const form = document.getElementsByTagName('form')[0];
-console.log(form);
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log('yes');
+  validateEmail();
 });
+
+const validateEmail = () => {
+  let email = document.getElementById('email').value;
+  let isValidEmail = false;
+  const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (email.length) {
+    if (email.match(emailFormat)) {
+      isValidEmail = true;
+    } else {
+    }
+  } else {
+  }
+  return isValidEmail;
+};
