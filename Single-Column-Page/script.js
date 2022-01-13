2e66cb2825141abd89ea943547d52fc0c804ebbd
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-13 11:34:52 -0800

    reworked variable logic

--- Single-Column-Page/script.js
+++ Single-Column-Page/script.js
@@ -2,17 +2,20 @@ const form = document.getElementsByTagName('form')[0];
 
 const validateEmail = (event) => {
   event.preventDefault();
-  let email = document.getElementById('input-box__email');
+
+  let email = document.getElementById('input-box__email').value;
+  let inputBox = document.getElementById('input-box__email');
+  let errorMessage = document.getElementById('error-message');
+
   let isValidEmail = false;
   const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (email.value.length) {
+
+  if (email.length) {
     if (email.match(emailFormat)) {
       isValidEmail = true;
     } else {
     }
   } else {
-    let inputBox = document.getElementById('input-box__email');
-    let errorMessage = document.getElementById('error-message');
     errorMessage.classList.add('active');
     errorMessage.innerText = 'Field cannot be blank';
 
