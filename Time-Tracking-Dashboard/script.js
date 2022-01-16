c01d2a333320a8fcaaa70e53971bcb4bd366554f
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-16 13:39:12 -0800

    populateDailyData() function

--- Time-Tracking-Dashboard/script.js
+++ Time-Tracking-Dashboard/script.js
@@ -12,4 +12,19 @@ const populateStaticData = async () => {
   }
 };
 
+const populateDailyData = async () => {
+  const data = await retrieveData();
+  const cards = document.getElementsByClassName('stat-card');
+  for (let i = 0; i < cards.length; i++) {
+    const currentData = data[i].timeframes.daily.current;
+    const previousData = data[i].timeframes.daily.previous;
+    cards[i].innerHTML = `<h1 class="stat-card__time">${currentData}hrs</h1>
+        <div class="stat-card__previous-time">Last Week - ${previousData}hrs</div>`;
+  }
+};
+
 populateStaticData();
+populateDailyData();
+
+const dailyButton = document.getElementById('btn-daily');
+dailyButton.addEventListener('click', populateDailyData);
