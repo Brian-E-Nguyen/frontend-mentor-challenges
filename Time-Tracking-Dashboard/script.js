96d6a1d042c31bda0b86628dbd1aeb709055d830
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-16 13:42:38 -0800

    populateMonthlyData function

--- Time-Tracking-Dashboard/script.js
+++ Time-Tracking-Dashboard/script.js
@@ -34,10 +34,23 @@ const populateWeeklyData = async () => {
   }
 };
 
+const populateMonthlyData = async () => {
+  const data = await retrieveData();
+  const cards = document.getElementsByClassName('stat-card');
+  for (let i = 0; i < cards.length; i++) {
+    const currentData = data[i].timeframes.monthly.current;
+    const previousData = data[i].timeframes.monthly.previous;
+    cards[i].innerHTML = `<h1 class="stat-card__time">${currentData}hrs</h1>
+        <div class="stat-card__previous-time">Last Week - ${previousData}hrs</div>`;
+  }
+};
+
 populateStaticData();
 populateDailyData();
 
 const dailyButton = document.getElementById('btn-daily');
 const weeklyButton = document.getElementById('btn-weekly');
+const monthlyButton = document.getElementById('btn-monthly');
 dailyButton.addEventListener('click', populateDailyData);
 weeklyButton.addEventListener('click', populateWeeklyData);
+monthlyButton.addEventListener('click', populateMonthlyData);
