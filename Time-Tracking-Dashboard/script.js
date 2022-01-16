6b805faf07d501c75d1c3947c318488d6edffc6a
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-16 14:04:54 -0800

    reworked population functions

--- Time-Tracking-Dashboard/script.js
+++ Time-Tracking-Dashboard/script.js
@@ -16,14 +16,14 @@ const populateDailyData = async () => {
   const data = await retrieveData();
   const cards = document.getElementsByClassName('stat-card');
   for (let i = 0; i < cards.length; i++) {
-    const currentData = data[i].timeframes.daily.current + 'hrs';
+    const currentData = data[i].timeframes.daily.current;
     const previousData = data[i].timeframes.daily.previous;
 
     let currentTime = document.getElementsByClassName('stat-card__time')[i];
     let previousTime = document.getElementsByClassName(
       'stat-card__previous-time'
     )[i];
-    currentTime.innerText = currentData;
+    currentTime.innerText = currentData + 'hrs';
     previousTime.innerText = `Last week - ${previousData}hrs`;
   }
 };
@@ -34,8 +34,13 @@ const populateWeeklyData = async () => {
   for (let i = 0; i < cards.length; i++) {
     const currentData = data[i].timeframes.weekly.current;
     const previousData = data[i].timeframes.weekly.previous;
-    cards[i].innerHTML = `<h1 class="stat-card__time">${currentData}hrs</h1>
-        <div class="stat-card__previous-time">Last Week - ${previousData}hrs</div>`;
+
+    let currentTime = document.getElementsByClassName('stat-card__time')[i];
+    let previousTime = document.getElementsByClassName(
+      'stat-card__previous-time'
+    )[i];
+    currentTime.innerText = currentData + 'hrs';
+    previousTime.innerText = `Last week - ${previousData}hrs`;
   }
 };
 
@@ -45,13 +50,18 @@ const populateMonthlyData = async () => {
   for (let i = 0; i < cards.length; i++) {
     const currentData = data[i].timeframes.monthly.current;
     const previousData = data[i].timeframes.monthly.previous;
-    cards[i].innerHTML = `<h1 class="stat-card__time">${currentData}hrs</h1>
-        <div class="stat-card__previous-time">Last Week - ${previousData}hrs</div>`;
+
+    let currentTime = document.getElementsByClassName('stat-card__time')[i];
+    let previousTime = document.getElementsByClassName(
+      'stat-card__previous-time'
+    )[i];
+    currentTime.innerText = currentData + 'hrs';
+    previousTime.innerText = `Last week - ${previousData}hrs`;
   }
 };
 
 populateStaticData();
-// populateDailyData();
+populateDailyData();
 
 const dailyButton = document.getElementById('btn-daily');
 const weeklyButton = document.getElementById('btn-weekly');
