c7c41713a67d3f34b90f0fa6245207e9b6c4706f
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-16 14:43:50 -0800

    reworked population functions

--- Time-Tracking-Dashboard/script.js
+++ Time-Tracking-Dashboard/script.js
@@ -11,52 +11,39 @@ const populateStaticData = async () => {
     titles[i].innerText = `${data[i].title}`;
   }
 };
+const displayData = async (currentData, previousData, i) => {
+  let currentTime = document.getElementsByClassName('stat-card__time')[i];
+  let previousTime = document.getElementsByClassName(
+    'stat-card__previous-time'
+  )[i];
+  currentTime.innerText = currentData + 'hrs';
+  previousTime.innerText = `Last week - ${previousData}hrs`;
+};
 
 const populateDailyData = async () => {
   const data = await retrieveData();
-  const cards = document.getElementsByClassName('stat-card');
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     const currentData = data[i].timeframes.daily.current;
     const previousData = data[i].timeframes.daily.previous;
-
-    let currentTime = document.getElementsByClassName('stat-card__time')[i];
-    let previousTime = document.getElementsByClassName(
-      'stat-card__previous-time'
-    )[i];
-    currentTime.innerText = currentData + 'hrs';
-    previousTime.innerText = `Last week - ${previousData}hrs`;
+    displayData(currentData, previousData, i);
   }
 };
 
 const populateWeeklyData = async () => {
   const data = await retrieveData();
-  const cards = document.getElementsByClassName('stat-card');
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     const currentData = data[i].timeframes.weekly.current;
     const previousData = data[i].timeframes.weekly.previous;
-
-    let currentTime = document.getElementsByClassName('stat-card__time')[i];
-    let previousTime = document.getElementsByClassName(
-      'stat-card__previous-time'
-    )[i];
-    currentTime.innerText = currentData + 'hrs';
-    previousTime.innerText = `Last week - ${previousData}hrs`;
+    displayData(currentData, previousData, i);
   }
 };
 
 const populateMonthlyData = async () => {
   const data = await retrieveData();
-  const cards = document.getElementsByClassName('stat-card');
-  for (let i = 0; i < cards.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     const currentData = data[i].timeframes.monthly.current;
     const previousData = data[i].timeframes.monthly.previous;
-
-    let currentTime = document.getElementsByClassName('stat-card__time')[i];
-    let previousTime = document.getElementsByClassName(
-      'stat-card__previous-time'
-    )[i];
-    currentTime.innerText = currentData + 'hrs';
-    previousTime.innerText = `Last week - ${previousData}hrs`;
+    displayData(currentData, previousData, i);
   }
 };
 
