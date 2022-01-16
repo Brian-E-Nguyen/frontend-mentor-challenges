e47deb5126e44c47e3501c7613dc149ebd2deb71
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-16 13:27:07 -0800

    static data populate

--- Time-Tracking-Dashboard/script.js
+++ Time-Tracking-Dashboard/script.js
@@ -4,5 +4,12 @@ const retrieveData = async () => {
   return data;
 };
 
-const data = retrieveData();
-console.log(data);
+const populateStaticData = async () => {
+  const data = await retrieveData();
+  const cards = document.getElementsByClassName('stat-card');
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].innerHTML = `<h1 class="stat-card__title">${data[i].title}</h1>`;
+  }
+};
+
+populateStaticData();
