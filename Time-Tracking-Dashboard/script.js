972ebb2322fa23ee31f5a63b6b97afd829c442b4
buraiyen <brian.edison.nguyen@gmail.com>
2022-01-16 14:02:57 -0800

    reworked populateDailyData

--- Time-Tracking-Dashboard/script.js
+++ Time-Tracking-Dashboard/script.js
@@ -6,9 +6,9 @@ const retrieveData = async () => {
 
 const populateStaticData = async () => {
   const data = await retrieveData();
-  const cards = document.getElementsByClassName('stat-card');
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].innerHTML = `<h1 class="stat-card__title">${data[i].title}</h1>`;
+  const titles = document.getElementsByClassName('stat-card__title');
+  for (let i = 0; i < titles.length; i++) {
+    titles[i].innerText = `${data[i].title}`;
   }
 };
 
@@ -16,10 +16,15 @@ const populateDailyData = async () => {
   const data = await retrieveData();
   const cards = document.getElementsByClassName('stat-card');
   for (let i = 0; i < cards.length; i++) {
-    const currentData = data[i].timeframes.daily.current;
+    const currentData = data[i].timeframes.daily.current + 'hrs';
     const previousData = data[i].timeframes.daily.previous;
-    cards[i].innerHTML = `<h1 class="stat-card__time">${currentData}hrs</h1>
-        <div class="stat-card__previous-time">Last Week - ${previousData}hrs</div>`;
+
+    let currentTime = document.getElementsByClassName('stat-card__time')[i];
+    let previousTime = document.getElementsByClassName(
+      'stat-card__previous-time'
+    )[i];
+    currentTime.innerText = currentData;
+    previousTime.innerText = `Last week - ${previousData}hrs`;
   }
 };
 
@@ -46,7 +51,7 @@ const populateMonthlyData = async () => {
 };
 
 populateStaticData();
-populateDailyData();
+// populateDailyData();
 
 const dailyButton = document.getElementById('btn-daily');
 const weeklyButton = document.getElementById('btn-weekly');
